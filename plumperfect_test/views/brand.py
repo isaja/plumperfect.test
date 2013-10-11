c4745ef77c6d96d50e1a3c4fd7c3333fb634513a
Isatou <isatousanneh14@hotmail.com>
2013-10-11 10:27:03 -0400

    tasks

--- plumperfect_test/views/brand.py
+++ plumperfect_test/views/brand.py
@@ -1,5 +1,6 @@
 from plumperfect_test.controllers import BrandCtrl
 from .base import RestView
+from flask.ext.classy import route #added by me
 
 class BrandView( RestView ):
 
@@ -7,3 +8,15 @@ class BrandView( RestView ):
 
     route_base = 'brand'
     Controller = BrandCtrl
+
+
+    @route('/<id>/')
+    @route('/<id>/<category>')
+    def get(self, id, category = None):
+        if category == None:
+	    results = self.controller.get(id)
+	elif category == 'product':
+	    results = self.controller.filter_product(id)
+	else:
+	    results = []
+	return self._json(results)
